1486aecace54bd6b7dd5e6621d23aecfb703700f
Eugene Titarchuk <etitarchuk@gmail.com>
2025-03-16 20:36:07 +0200

    added consumer-producer task

---
+++ pr3/src/DSPC.ConsumerProducer/DSPC.ConsumerProducer/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace DSPC.ConsumerProducer
+{
+    public class Logger : IAsyncDisposable
+    {
+        private StreamWriter _output;
+
+        public string Filename { get; private set; }
+
+        public Logger()
+        {
+            Filename = GenerateFileName();
+
+            var logFile = new FileStream(Filename, FileMode.CreateNew, FileAccess.Write, FileShare.Read);
+            
+            _output = new StreamWriter(logFile, Encoding.UTF8);
+        }
+
+        public void LogMessage(string logLevel, string message)
+        {
+            var logMessage = FromatMessage(DateTime.Now, logLevel, message);
+
+            _output.WriteLine(logMessage);
+            _output.Flush();
+        }
+
+        private string FromatMessage(DateTime time, string logLevel, string message)
+        {
+            return $"{time:dd/MM/yyyy HH:mm:ss.fff} [{logLevel}] {message}";
+        }
+
+        private string GenerateFileName()
+        {
+            return $"logfile_{DateTime.Now:ddMM_HHmmss_fff}.log";
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _output.FlushAsync();
+            await _output.DisposeAsync();
+        }
+    }
+}
---
+++ pr3/src/DSPC.ConsumerProducer/DSPC.ConsumerProducer/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace DSPC.ConsumerProducer
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var logger = new Logger();
+
+            Console.WriteLine($"Log file created: {Path.GetFullPath(logger.Filename)}");
+
+            var a = new Producer(logger, "A");
+            var b = new Producer(logger, "B");
+            var c = new Producer(logger, "C");
+
+            a.StartProduce();
+            b.StartProduce();
+            c.StartProduce();
+
+            Console.WriteLine("Press <enter> to stop...");
+
+            Console.ReadLine();
+
+            a.StopProduce();
+            b.StopProduce();
+            c.StopProduce();
+
+            Console.WriteLine("Done!");
+        }
+    }
+
+    public class Producer
+    {
+        private readonly Logger _logger;
+        private readonly string _name;
+        private readonly Random _random;
+        private bool _cancelled;
+
+        public Producer(Logger logger, string name)
+        {
+            _random = new Random();
+
+            _logger = logger;
+            _name = name;
+        }
+
+        public void StartProduce()
+        {
+            var thread = new Thread(Produce);
+
+            thread.Start();
+        }
+
+        public void StopProduce()
+        {
+            _cancelled = true;
+        }
+
+        private void Produce()
+        {
+            while (!_cancelled)
+            {
+                _logger.LogMessage("Debug", $"Message from the Producer {_name}");
+                Thread.Sleep(_random.Next(50, 100));
+            }
+        }
+    }
+}
